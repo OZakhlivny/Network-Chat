80597f929714035d6464837e29a5a243cf0aab5f
OZakhlivny <61385122+OZakhlivny@users.noreply.github.com>
2021-05-06 16:45:18 +0300

    Delete src/lesson1 directory

--- src/lesson1/Activity.java
+++
@@ -1,7 +0,0 @@
-package lesson1;
-
-public interface Activity {
-    String getInfo();
-    int run();
-    int jump ();
-}
--- src/lesson1/Let.java
+++
@@ -1,5 +0,0 @@
-package lesson1;
-
-public interface Let {
-    boolean overcome(Activity object);
-}
