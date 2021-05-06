7192afee9814de3f6c9cd68de7acdbd3938eee76
OZakhlivny <61385122+OZakhlivny@users.noreply.github.com>
2021-05-06 16:45:32 +0300

    Delete src directory

--- src/lesson2/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package lesson2;
-
-public class MyArrayDataException extends NumberFormatException {
-    public MyArrayDataException(String errorString){
-
-        super(String.format("Ошибка данных массива! %s", errorString));
-    }
-}
--- src/lesson2/MyArraySizeException.java
+++
@@ -1,8 +0,0 @@
-package lesson2;
-
-public class MyArraySizeException extends ArrayIndexOutOfBoundsException{
-    MyArraySizeException(String errorString)
-    {
-        super(String.format("Ошибка размерности массива! %s", errorString));
-    }
-}
